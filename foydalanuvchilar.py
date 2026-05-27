8ab4bf8d6bf2f49144d48f5ffb8d1a8744576fff
OzodbekCoder1 <nnomalum777@gmail.com>
2026-05-27 10:47:35 +0500

    foydalanuvchilar qismidagi yosh tekshiruvi togirlandi

--- foydalanuvchilar.py
+++ foydalanuvchilar.py
@@ -3,6 +3,7 @@ import re
 def promo_kod_tekshiruv(kod: str) -> bool:
     return bool(kod_tekshirish = re.search(r'^[A-Z]+_{1}[0-9]+$',kod))
 
-def mijoz_yoshini_tekshir(bemor_yoshi: int, film_cheklovi: str) -> bool:
-    return bemor_yoshi >= film_cheklovi["yosh_cheklovi"]
+def mijoz_yoshini_tekshir(yosh: int, film_cheklovi: str) -> bool:
+    cheklov_son = int(film_cheklovi.replace("+", ""))
+    return yosh >= cheklov_son
 
